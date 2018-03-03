ccff0384293b5ea42eccb169fed33dc43648791a
Cesar Valenzuela <kaiser122@gmail.com>
2018-03-03 16:13:15 -0700

    initial commit

---
+++ src/com/cesarvalenzuela/Main.java
@@ -0,0 +1,15 @@
+package com.cesarvalenzuela;
+
+/**
+ * Author: Cesar Valenzuela
+ * Date: 3/3/2018
+ * Course:
+ * Assignment:
+ * Instructor:
+ * T.A:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
---
+++ src/com/cesarvalenzuela/Square.java
@@ -0,0 +1,12 @@
+package com.cesarvalenzuela;
+
+/**
+ * Author: Cesar Valenzuela
+ * Date: 3/3/2018
+ * Course:
+ * Assignment:
+ * Instructor:
+ * T.A:
+ */
+public class Square {
+}
